1c60a1e16212e9d12d07e0b7df1f1d3adb8eb7a5
Reinaldo <reinaldoper83@gmail.com>
2022-05-19 10:13:51 -0300

    função catAndMouse

--- src/challenges.js
+++ src/challenges.js
@@ -74,9 +74,20 @@ function highestCount(numeros) {
 
 
 // Desafio 7
-function catAndMouse() {
+function catAndMouse(mouse, cat1, cat2) {
   // seu código aqui
+ 
+  if((cat1+=mouse) < (cat2+=mouse)){
+    return 'cat1';
+  }
+  if((cat1+=mouse) > (cat2+=mouse)){
+    return 'cat2';
+  }
+  if((cat1+=mouse) === (cat2+=mouse)){
+    return 'os gatos trombam e o rato foge';
+  }
 }
+
 
 // Desafio 8
 function fizzBuzz() {
